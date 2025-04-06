3be4b301780c3e09bc841a99e1868bec9879fb2b
Ashish <kapoorashish713@gmail.com>
2025-04-06 12:43:07 +0530

    a

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const Port = 5000;
 
 //cors policy
 const corsOption ={
-    origin: ["http://localhost:5173","https://eduviibe.netlify.app","https://eduvibe-smoky.vercel.app"],
+    origin: ["http://localhost:5173","https://eduvibe-smoky.vercel.app"],
     method:"GET,POST,PUT,DELETE,PATCH,HEAD",
     credentials: true,
 };
